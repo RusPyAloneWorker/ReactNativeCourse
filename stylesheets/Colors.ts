796951aa63236d3a2c2c268e5d241169fd4e7e36
ruspyaloneworker <DioBrando2003@yandex.ru>
2025-01-17 17:23:09 +0300

    9 и 10 домашние задания

---
+++ stylesheets/Colors.ts
@@ -0,0 +1,24 @@
+import {ColorsKeys, IColorsValues} from "./theme/ThemeTypes";
+
+export const Colors: Record<ColorsKeys, IColorsValues> = {
+	overlay: {
+		light: '#21212114',
+		dark: '#FFFFFF14',
+	},
+	backgroundPrimary: {
+		light: '#EBEBEB',
+		dark: '#121212',
+	},
+	accentDefault: {
+		light: '#1B58F1',
+		dark: '#062369',
+	},
+	textPrimary: {
+		light: '#000',
+		dark: '#FFFFFF',
+	},
+	textSecondary: {
+		light: '#363434',
+		dark: '#969393',
+	},
+};
